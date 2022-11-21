261bd32f35649b1b8dbe79ab128f3c8e908a4002
RyanReedKnight <ryanreedknight@gmail.com>
2022-11-20 21:46:53 -0800

    Comments added to TicketStatus

--- src/main/java/com/revature/EmployeeTicketApplication/Models/TicketStatus.java
+++ src/main/java/com/revature/EmployeeTicketApplication/Models/TicketStatus.java
@@ -1,5 +1,8 @@
 package com.revature.EmployeeTicketApplication.Models;
 
+/**
+ * Enum to categorize Ticket instances as pending, approved or declined. Tickets are pending by default.
+ * */
 public enum TicketStatus {
     PENDING,
     APPROVED,
